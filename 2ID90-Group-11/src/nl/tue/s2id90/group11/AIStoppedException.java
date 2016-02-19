fd14f2bbc4189376d60d316bb8c9499024838368
mthaak <martin.ter.haak@gmail.com>
2016-02-19 01:46:32 +0100

    Started implementation StrongPlayer

---
+++ 2ID90-Group-11/src/nl/tue/s2id90/group11/AIStoppedException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.tue.s2id90.group11;
+
+/**
+ *
+ * @author s136279
+ */
+class AIStoppedException extends Exception {
+
+    public AIStoppedException() {
+    }
+
+}
