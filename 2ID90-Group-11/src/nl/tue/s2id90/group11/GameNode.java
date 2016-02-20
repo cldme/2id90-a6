9a42fea2364df0ae4f35304141f7276534c396ca
mthaak <martin.ter.haak@gmail.com>
2016-02-21 00:23:57 +0100

    Simplified, moved evaluation to player

--- 2ID90-Group-11/src/nl/tue/s2id90/group11/GameNode.java
+++ 2ID90-Group-11/src/nl/tue/s2id90/group11/GameNode.java
@@ -1,84 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nl.tue.s2id90.group11;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import nl.tue.s2id90.draughts.DraughtsState;
-import nl.tue.s2id90.game.GameState;
 import org10x10.dam.game.Move;
-import java.util.Arrays;
 
 /**
- *
+ * Node in decision tree.
  * @author Martin
  */
 public class GameNode {
-
     private DraughtsState state;
     private Move bestMove;
-    private int depth;
+    private int depth;    
 
-    private final int MAX_DEPTH = 10;
-
-    public GameNode(DraughtsState s, int d) {
+    public GameNode(DraughtsState s, int d){
         this.state = s;
         this.depth = d;
     }
 
-    DraughtsState getGameState() {
+    public DraughtsState getGameState() {
         return this.state;
     }
 
-    void setBestMove(Move m) {
-        this.bestMove = m;
-    }
-
-    Move getBestMove() {
+    public Move getBestMove() {
         return this.bestMove;
     }
 
-    boolean isLeaf() {
-        return this.state.isEndState() || this.depth >= this.MAX_DEPTH;
-    }
-
-    int evaluate(boolean isWhite) {
-        int count = 0;
-        for (int piece : this.state.getPieces()) {
-            if (piece == 1) {
-                if (isWhite) {
-                    count += 1;
-                } else {
-                    count -= 1;
-                }
-            } else if (piece == 2) {
-                if (isWhite) {
-                    count -= 1;
-                } else {
-                    count += 1;
-                }
-            } else if (piece == 3) {
-                if (isWhite) {
-                    count += 2;
-                } else {
-                    count -= 2;
-                }
-            } else if (piece == 4) {
-                if (isWhite) {
-                    count -= 2;
-                } else {
-                    count += 2;
-                }
-            }
-        }
-        return count;
-    }
-
-    int getDepth() {
+    public void setBestMove(Move m) {
+        this.bestMove = m;
+    }    
+    
+    public int getDepth() {
         return this.depth;
     }
-
 }
